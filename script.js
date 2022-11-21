11546057a1a2fb71dafdd5589dc1ba56c3d9ae00
Johnk298 <johnk298@gmatc.matc.edu>
2022-11-20 23:47:36 -0600

    trying to adding Consonant counter

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 var vowelCount = document.getElementById('vowelCount');
 
-function countVowel(str){
-    str = str.toLowerCase();
+function countVowel(vowelArr){
+    vowelArr = vowelArr.toLowerCase();
     let vowArr=['a','e','i','o','u'],
         counter=0;
 
-        for(let letter of str){
+        for(let letter of vowelArr){
             if(vowArr.includes(letter)){
                 counter++;
                 vowelCount.classList.add('blink');
@@ -15,4 +15,23 @@ function countVowel(str){
             }
         }
         vowelCount.innerHTML= counter;
+}
+// consonants//
+var constCount = document.getElementById('constCount');
+
+function countConst(constArr){
+    constArr = constArr.toLowerCase();
+    let conArr=['b','c','d','f','g','h','j','k','f','l','m','n','p','q','r','s','t','v','w','x','y','z'],
+        counter=0;
+
+        for(let x of constArr){
+            if(conArr.includes(x)){
+                counter++;
+                constCount.classList.add('blink');
+            }
+            else{
+                constCount.classList.remove('blink');
+            }
+        }
+        constCount.innerHTML= counter;
 }
